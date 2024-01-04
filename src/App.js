53eae48c11445c34feb03577efcca0d03fa8fece
Edward Shanahan <edwardshanahan357@gmail.com>
2024-01-04 20:40:33 +0000

    Add Hero component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import Container from "./components/Container/Container";
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
   return (
     <Container>
       <Navbar />
+      <Hero />
     </Container>
   );
 };
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="Hero">
+      <h1>Design solutions made easy</h1>
+      <p>
+        With over ten years of experience in various design disciplines, I’m
+        your one-stop shop for your design needs.
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
