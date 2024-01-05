9974b5a8651412eb4f8eff170516a71c9dec6f7d
Edward Shanahan <edwardshanahan357@gmail.com>
2024-01-05 17:32:08 +0000

    Add Services Component and assets

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Container from "./components/Container/Container";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services";
 
 const App = () => {
   return (
     <Container>
       <Navbar />
       <Hero />
+      <Services />
     </Container>
   );
 };
---
+++ src/components/Services/Services.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PatternApps from "./pattern-apps.svg";
+import PatternGraphic from "./pattern-graphic-design.svg";
+import PatternIllustrations from "./pattern-illustrations.svg";
+import PatternMotion from "./pattern-motion-graphics.svg";
+import PatternPhotography from "./pattern-photography.svg";
+import PatternUI from "./pattern-ui-ux.svg";
+
+const Services = () => {
+  return (
+    <div className="Services">
+      <h1>Services!</h1>
+    </div>
+  );
+};
+
+export default Services;
