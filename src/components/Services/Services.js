d9d00f8e2f15d520bee68319ac4ebb7cc8ad82fe
Edward Shanahan <edwardshanahan357@gmail.com>
2024-01-06 14:55:25 +0000

    Add service content

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -9,7 +9,41 @@ import PatternUI from "./pattern-ui-ux.svg";
 const Services = () => {
   return (
     <div className="Services">
-      <h1>Services!</h1>
+      <div className="service service-1">
+        <img src={PatternGraphic} alt="Graphic Pattern" />
+
+        <h3>Graphic design</h3>
+      </div>
+
+      <div className="service service-2">
+        <img src={PatternUI} alt="UI/UX Pattern" />
+
+        <h3>UI/UX</h3>
+      </div>
+
+      <div className="service service-3">
+        <img src={PatternApps} alt="Apps Pattern" />
+
+        <h3>Apps</h3>
+      </div>
+
+      <div className="service service-4">
+        <img src={PatternIllustrations} alt="Illustrations Pattern" />
+
+        <h3>Illustrations</h3>
+      </div>
+
+      <div className="service service-5">
+        <img src={PatternPhotography} alt="Photography Pattern" />
+
+        <h3>Photography</h3>
+      </div>
+
+      <div className="service service-6">
+        <img src={PatternMotion} alt="Motion Pattern" />
+
+        <h3>Motion Graphics</h3>
+      </div>
     </div>
   );
 };
